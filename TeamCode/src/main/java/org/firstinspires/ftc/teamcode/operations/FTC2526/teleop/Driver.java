2cbd3883636d15c5d7a7d3804ccef949abb81f5d
June <68359573+CT1138@users.noreply.github.com>
2025-10-04 17:07:21 -0700

    goon 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/operations/FTC2526/teleop/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/operations/FTC2526/teleop/Driver.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -12,11 +13,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.core.util.Mecanum;
 
 import java.io.IOException;
-import java.nio.channels.Selector;
 
-@Disabled
-@TeleOp(name="Slingshotter", group="Decode")
-public class Slingshotter extends OpMode
+@TeleOp(name="Driver", group="Decode")
+public class Driver extends OpMode
 {
     // define device classes
     Mecanum moMecanum = new Mecanum(0.75);
@@ -25,17 +24,9 @@ public class Slingshotter extends OpMode
     private DcMotor moDrive_FrontRight = null;
     private DcMotor moDrive_RearLeft = null;
     private DcMotor moDrive_RearRight = null;
-    private DcMotor moAux_Flywheel = null;
-    private DcMotor moAux_Selector = null;
-    private Servo soAux_Hammer = null;
-    private Servo soAux_GrabberWrist = null;
-    private Servo soAux_GrabberClaw = null;
-    private TouchSensor stoAux_Positioner = null;
-
-    private boolean mbMovingChamber = false;
 
     // throws IOException as some utility classes I wrote require file operations
-    public Slingshotter() throws IOException {
+    public Driver() throws IOException {
     }
 
     // Prep our motors and servos
@@ -52,26 +43,8 @@ public class Slingshotter extends OpMode
         moDrive_RearRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         moDrive_RearLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Aux Setup
-        // Flywheel: Motor that spins at a high RPM to propel the Artefact on contact
-        // Selector: Motor that cycles the artefacts in our Revolver Chamber
-        // Hammer: Servo that pushes the artefact into the flywheel
-        // Positioner: Touch Sensor to tell what position the chamber is in
-        //              (When one of the fins is at 12:00, the sensor is pressed)
-        //moAux_Flywheel = hardwareMap.get(DcMotor.class, "flywheel");
-        //moAux_Selector = hardwareMap.get(DcMotor.class, "selector");
-        //soAux_Hammer = hardwareMap.get(Servo.class, "hammer");
-        //soAux_GrabberWrist = hardwareMap.get(Servo.class, "wrist");
-        //soAux_GrabberClaw = hardwareMap.get(Servo.class, "claw");
-        //stoAux_Positioner = hardwareMap.get(TouchSensor.class, "Positioner");
-
-        // We will use positions to determine where the chamber needs to be, so the robot must
-        //      be in a constant starting position
-        //moAux_Selector.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //moAux_Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //moAux_Selector.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //moAux_Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
+        moDrive_FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        moDrive_RearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     // Reset our timer
@@ -80,58 +53,21 @@ public class Slingshotter extends OpMode
         moRuntime.reset();
     }
 
-    private void operator() {
-        Gamepad gpOperator = gamepad2;
-        // 9/13/2025 - This code is purely theoretical as we havent built the robot features itself yet
-        int ChamberIndex = 0;
-        int[] ChamberPositions = {30, 60, 90};
-        double FlywheelPower = 0;
-        int SelectorPosition = 0;
-        int HammerPosition = 0;
-        int WristPosition = 0;
-        int ClawPosition = 0;
-
-        // Grabber Controls
-        boolean mbGrab = gpOperator.right_bumper;
-        boolean mbFlipper = gpOperator.left_bumper;
-
-        // Launcher Controls
-        boolean mbStopFlywheel = gpOperator.dpad_down;
-        boolean mbHammer = gpOperator.dpad_up;
-        boolean mbCycleUp = gpOperator.dpad_left;
-        boolean mbCycleDown = gpOperator.dpad_right;
-
-        // Begin Functions
-        if (!mbStopFlywheel) {
-            FlywheelPower = 1;
-        }
-        if (mbHammer) {
-            HammerPosition = 1;
-        }
-
-        // Pos by Pos solution for Revolving Chamber
-        int cycleUpInt = mbCycleUp ? 1 : 0;
-        int cycleDownInt = mbCycleDown ? 1 : 0;
-        ChamberIndex = ChamberIndex + (cycleUpInt - cycleDownInt);
-        SelectorPosition = ChamberPositions[ChamberIndex];
-
-        moAux_Flywheel.setPower(FlywheelPower);
-        moAux_Selector.setTargetPosition(SelectorPosition);
-    }
-
     private void driver() {
         Gamepad gpDriver = gamepad1;
         // Controls definition
         double mdDrive = gpDriver.left_stick_y;
         double mdStrafe = -gpDriver.left_stick_x;
-        double mdTwist = gpDriver.right_stick_x;
+        double mdTwist = -gpDriver.right_stick_x;
         double mdBrake = gpDriver.right_trigger;
         boolean mbDriveLeft = gpDriver.dpad_left;
         boolean mbDriveRight = gpDriver.dpad_right;
         boolean mbDriveUp = gpDriver.dpad_up;
         boolean mbDriveDown = gpDriver.dpad_down;
-        boolean mbTwistLeft = gpDriver.x;
-        boolean mbTwistRight = gpDriver.b;
+        boolean mbTwistLeft = gpDriver.left_bumper;
+        boolean mbTwistRight = gpDriver.right_bumper;
+
+
 
         // Button based controls, use mdBrake to adjust speed
         if (mbDriveLeft) mdStrafe = 1 - mdBrake;
@@ -139,12 +75,13 @@ public class Slingshotter extends OpMode
         if (mbDriveUp) mdDrive = -1 + mdBrake;
         if (mbDriveDown) mdDrive = 1 - mdBrake;
 
-        if (mbTwistLeft) mdTwist = -1 + mdBrake;
-        if (mbTwistRight) mdTwist = 1 - mdBrake;
+        if (mbTwistRight) mdTwist = -1 - mdBrake;
+        if (mbTwistLeft) mdTwist = 1 + mdBrake;
+
 
         // Calculate the target power for all wheels assuming a mecanum drivetrain is in use
         // see teamcode.core.util.Mecanum
-        double[] wheelpower = moMecanum.Calculate(mdTwist, mdStrafe, -mdDrive, gamepad2.right_bumper);
+        double[] wheelpower = moMecanum.Calculate(mdDrive, mdStrafe, mdTwist, gamepad2.right_bumper);
 
         moDrive_FrontLeft.setPower(wheelpower[0]);
         moDrive_FrontRight.setPower(wheelpower[1]);
@@ -158,6 +95,7 @@ public class Slingshotter extends OpMode
         telemetry.addLine("DriveTrain");
         telemetry.addLine("===================================");
         telemetry.addData("Front Left Power", moDrive_FrontLeft.getPower());
+        telemetry.addLine("");
         telemetry.addData("Front Right Power", moDrive_FrontRight.getPower());
         telemetry.addData("Rear Left Power", moDrive_RearLeft.getPower());
         telemetry.addData("Rear Right Power", moDrive_RearRight.getPower());
@@ -175,7 +113,6 @@ public class Slingshotter extends OpMode
     @Override
     public void loop() {
         driver();
-        operator();
         telecom();
     }
 
